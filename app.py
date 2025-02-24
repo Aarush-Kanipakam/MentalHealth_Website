73dd478695e9b6c3b3424d5a4518ef029ffdba95
Aarush-Kanipakam <157634160+Aarush-Kanipakam@users.noreply.github.com>
2025-02-24 17:33:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ from supabase import create_client, Client
 import uuid
 import logging
 import pickle
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import tensorflow as tf
+tf.config.experimental.set_memory_growth(tf.config.list_physical_devices('CPU')[0], True)
+
 import time
 import subprocess
 import tempfile
@@ -1088,4 +1091,5 @@ def predict():
         return jsonify({"error": "An error occurred during prediction"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port, debug=True)
